a081489e861274778e3722bd0b79a7d843232c31
kp63 <46412342+kp63@users.noreply.github.com>
2025-03-21 07:39:26 +0900

    asgi.pyを修復

--- config/asgi.py
+++ config/asgi.py
@@ -3,12 +3,15 @@ from django.core.management import call_command
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import config.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+django_asgi_application = get_asgi_application()
+
+import config.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_application,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             config.routing.websocket_urlpatterns
