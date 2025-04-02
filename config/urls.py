c2b70782462a49f88d9446a5450926b5b2622df4
kamok <kamoken0531@gmail.com>
2025-04-02 10:23:58 +0900

    PWA化

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,11 @@
+import os
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from config.settings import BASE_DIR
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +14,6 @@ urlpatterns = [
     path('', include('morisummon.urls')),
     path('', include('battle.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'static'))
